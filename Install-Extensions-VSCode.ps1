563297769e721ed14a59b26f9ec36c328f6c5b89
Soham Ray <102452424+sysmonyx@users.noreply.github.com>
2024-03-16 19:14:42 +0530

    Added Extensions & Others

--- Install-Extensions-VSCode.ps1
+++ Install-Extensions-VSCode.ps1
@@ -13,7 +13,7 @@
 ################################################################################################################################
 #   Description:    This script installs the specified Visual Studio Code Extensions automatically.
 #
-#   Last Updated:   30.04.2023
+#   Last Updated:   16.03.2024
 ################################################################################################################################
 # Elevate script to have Administrative privileges.
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
@@ -23,29 +23,43 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $Extensions = @(
 
     # Sorted A-Z.
-    "eliverlara.andromeda"                                      # Andromeda Theme
     "formulahendry.auto-close-tag"                              # Auto Close Tag
-    "visualstudioexptteam"                                      # Intellicode
+    "ms-vscode.cpptools"                                        # C/C++
+    "ms-vscode.cpptools-extension-pack"                         # C/C++ Extension Pack
+    "ms-dotnettools.csharp"                                     # C#
+    "gitlab.gitlab-workflow"                                    # GitLab Workflow
+    "sidthesloth.html5-boilerplate"                             # HTML-5 Boilerplate
+    "ecmel.vscode-html-css"                                     # HTML CSS Support
+    "oderwat.indent-rainbow"                                    # Indent Rainbow
+    "visualstudioexptteam.vscodeintellicode"                    # Intellicode
+    "redhat.java"                                               # Language Support for JAVA
     "ritwickdey.liveserver"                                     # Live Server
-    "pkief.material-icon-theme"                                 # Material Icon Theme
+    "shd101wyy.markdown-preview-enhanced"                       # Markdown Preview Enhanced
     "christian-kohler.path-intellisense"                        # Path Intellisense
     "pnp.polacode"                                              # Polacode by P&P
     "ms-vscode.PowerShell"                                      # Powershell
     "esbenp.prettier-vscode"                                    # Prettier
+    "ms-python.vscode-pylance"                                  # Pylance
     "ms-python.python"                                          # Python
     "ionutvmi.reg"                                              # REG
     "ms-vscode-remote.remote-ssh"                               # Remote - SSH
     "ms-vscode-remote.remote-ssh-edit"                          # Remote - SSH: Editing Configuration Files
     "ms-vscode.remote-explorer"                                 # Remote Explorer
     "redhat.vscode-xml"                                         # XML
+    "dotjoshjohnson.xml"                                        # XML Tools
     "redhat.vscode-yaml"                                        # YAML
+
+    # Themes.
+    "antfu.icons-carbon"                                        # Carbon Product Theme
+    "pkief.material-icon-theme"                                 # Material Icon Theme
+    "joelcrosby.one-dark-darker"                                # One Dark Darker
 
 )
 
 # Install the Extensions.
 foreach ($Extension in $Extensions)
 {
-	 & "$env:ProgramFiles\Microsoft VS Code\bin\code.cmd" --install-extension $Extension
+	code --install-extension $Extension
 }
 
 # Wait for Key-Press from user.
